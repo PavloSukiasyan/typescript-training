ef5b4d52d22d5bee612736bf2ccd95dc4b277a5e
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2023-06-22 12:10:49 +0300

    TS-T-2: 2 new cases - mirror string, pin case.

---
+++ src/kata_dir/test_1_sombra_mirror.ts
@@ -0,0 +1,18 @@
+
+// 'aba' -> True
+// 'avv' -> False
+
+function isMirrored(str: string): boolean {
+  const length = str.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (str[i] !== str[length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log('test1: ', isMirrored("aba") )
+console.log('test2: ', isMirrored("abaX") )
+console.log('test3: ', isMirrored("aba=") )
+console.log('test4: ', isMirrored("is si") )
+

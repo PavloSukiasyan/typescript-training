d94835d12aae157f2aa37cccb552aa5187d04c6e
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2024-06-09 18:56:42 +0300

    TS-T-12: basic op kata + renaming

--- src/24_kata/math_themed/opposites_attracts.ts
+++ src/24_kata/math_themed/opposites_attracts.ts
@@ -7,7 +7,7 @@
 
 export function lovefunc(flower1: number, flower2: number): boolean {
   return (flower1 % 2 === 0) == (flower2 % 2 === 1) ? true : false;
-  // from comments : 
+  // from comments :
   // return (flower1 + flower2) % 2 === 1;
 }
 

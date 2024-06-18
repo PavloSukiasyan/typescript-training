0736a178555694527e237daffe15aec38593bb80
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2024-06-18 13:51:05 +0300

    TS-T-28: 6 kyu  Persistent Bugger and easier ones

---
+++ src/24_kata/strings/dna_to_rna.ts
@@ -0,0 +1,42 @@
+/**
+ * 
+ * 8 kyu DNA to RNA Conversion
+ * 
+ * Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems.
+ *  It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. 
+RNA differs slightly from DNA its chemical structure and contains no Thymine.
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+ */
+
+export function DNAtoRNA(dna: string): string {
+  return [...dna]
+    .map(letter => {
+      if (letter.toUpperCase() === 'T') {
+        letter = 'U';
+      }
+      return letter;
+    })
+    .join('');
+}
+
+console.log('"GCAT"  =>  "GCAU"', DNAtoRNA('GCAT'));
+console.log('"TTTT"  =>  "UUUU"', DNAtoRNA('TTTT'));
+
+// other solution
+export function DNAtoRNA2(dna: string): string {
+  return dna.replace(/T/g, 'U');
+}
+console.log('\nversion 2');
+console.log('"GCAT"  =>  "GCAU"', DNAtoRNA2('GCAT'));
+console.log('"TTTT"  =>  "UUUU"', DNAtoRNA2('TTTT'));
